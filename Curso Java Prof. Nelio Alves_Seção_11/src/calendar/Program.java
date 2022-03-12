c6914165ab14f176e6e290e9f0d4eb98cd364d0c
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-03-12 15:53:25 -0300

    Seção 11 - Tópicos especiais em Java - Trabalhando com Datas

---
+++ Curso Java Prof. Nelio Alves_Seção_11/src/calendar/Program.java
@@ -0,0 +1,27 @@
+package calendar;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Program {
+    public static void main(String[] args) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+        System.out.println(sdf.format(d));
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        cal.add(Calendar.HOUR_OF_DAY, 4);
+        d = cal.getTime();
+        int minutes = cal.get(Calendar.MINUTE);
+        //No Calendar janeiro começa no mês 0 por esse motivo se coloca o +1 antes
+        int month = 1 + cal.get(Calendar.MONTH);
+
+        System.out.println(sdf.format(d));
+        System.out.println("Minutes: " + minutes);
+        System.out.println("Month: " + month);
+    }
+}
